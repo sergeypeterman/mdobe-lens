42ec13cb6b47b90244e0e2509f17e37f667c855c
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-06-19 19:03:32 +0100

    added menu and search options

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,33 +14,139 @@ import {
   faSliders,
 } from "@fortawesome/free-solid-svg-icons";
 
-const TYPES = {
-  1: { icon: faCamera, title: "Photo" },
-  2: { icon: faDesktop, title: "Illustration" },
-  3: { icon: faPenNib, title: "Vector" },
-  4: { icon: faVideo, title: "Video" },
-  6: { icon: faCube, title: "3D" },
-  7: { icon: faPenSquare, title: "Template" },
+const CONTENT_TYPES = [
+  { name: 1, icon: faCamera, title: "Photo" },
+  { name: 2, icon: faDesktop, title: "Illustration" },
+  { name: 3, icon: faPenNib, title: "Vector" },
+  { name: 4, icon: faVideo, title: "Video" },
+  { name: 5, icon: faCube, title: "3D" },
+  { name: 6, icon: faPenSquare, title: "Template" },
+];
+
+const ORDER = [
+  { name: "nb_downloads", title: "Downloads" },
+  { name: "relevance", title: "Relevance" },
+  { name: "creation", title: "Creation Date" },
+  { name: "featured", title: "Featured" },
+  { name: "undiscovered", title: "Undiscovered" },
+  { name: "undiscovered", title: "Undiscovered" },
+];
+//[age]=1w, 1m, 6m, 1y, 2y, 3y
+const AGE = [
+  { name: "1w", title: "1 week" },
+  { name: "1m", title: "1 month" },
+  { name: "6m", title: "6 months" },
+  { name: "1y", title: "1 year" },
+  { name: "2y", title: "2 years" },
+  { name: "3y", title: "3 years" },
+];
+const SETTINGS_TYPES = {
+  order: {
+    name: "order",
+    type: "radio",
+    caption: "Order by:",
+    values: ORDER,
+    selected: 0,
+  },
+  content: {
+    name: "media_type_id",
+    type: "checkbox",
+    caption: "Content types:",
+    values: CONTENT_TYPES,
+    selected: [true, true, true, true, false, false],
+  },
+  age: {
+    name: "age",
+    type: "radio",
+    caption: "Uploaded in the last:",
+    values: AGE,
+    selected: 2,
+  },
 };
 
-function Settings({ handleQuery, isEnter, query, handleClick }) {
+function SettingsBlock({ type, settingsValues, setSettingsValues }) {
+  let thisSetting = settingsValues[type];
+
+  const handleBlockChange = (e) => {
+    let newSet = JSON.parse(JSON.stringify(settingsValues));
+    let num = +e.target.value;
+    switch (thisSetting.type) {
+      case "radio":
+        newSet[type].selected = num;
+        break;
+      case "checkbox":
+        newSet[type].selected[num] = !newSet[type].selected[num];
+        break;
+    }
+    setSettingsValues(newSet);
+  };
+
+  return (
+    <div className="bg-gray-100 hover:bg-gray-50 px-4 py-1 m-1 rounded-lg shadow-lg ">
+      <fieldset className="">
+        <legend className="font-bold text-lg">{thisSetting.caption}</legend>
+
+        {thisSetting.values.map((item, ind) => {
+          let checked = false;
+          switch (thisSetting.type) {
+            case "radio":
+              checked = ind === thisSetting.selected;
+              break;
+            case "checkbox":
+              checked = thisSetting.selected[ind];
+              break;
+          }
+          return (
+            <div key={`${item.name}`} className="flex">
+              <input
+                type={thisSetting.type}
+                id={`${item.name}`}
+                value={`${ind}`}
+                name={thisSetting.name}
+                onChange={handleBlockChange}
+                checked={checked}
+              />
+
+              <label htmlFor={`${item.name}`} className="px-2 py-1">
+                {item.title}
+              </label>
+            </div>
+          );
+        })}
+      </fieldset>
+    </div>
+  );
+}
+
+function Settings({ handleQuery, isEnter, query, handleFetchClick }) {
+  const [settingsShow, setSettingsShow] = useState(false);
+  const [settingsValues, setSettingsValues] = useState(SETTINGS_TYPES);
+
+  const handleSettingsFilter = () => {
+    setSettingsShow(!settingsShow);
+  };
+
   return (
     <div
       id="search-form"
-      className="w-full flex flex-col items-center fixed top-0 left-0 px-12 py-8 
-               backdrop-blur-sm bg-gradient-to-b from-neutral-400"
+      className={`w-full flex flex-col items-center fixed top-0 left-0 px-12 py-8 
+      backdrop-blur-sm bg-gradient-to-b from-neutral-400`}
     >
       <div id="search-field" className="w-full flex flex-row items-center">
-        
         <button
+          id="search-settings"
           className="bg-gray-100 hover:bg-gray-200 text-center 
-          px-3 py-1 ml-1 -mr-1 text-gray-400 rounded-l-lg text-lg shadow-md "
-          
+          px-3 py-1 ml-1 -mr-1 text-gray-400 rounded-l-lg text-lg "
+          onClick={handleSettingsFilter}
         >
-          <FontAwesomeIcon icon={faSliders} className="" />
+          <FontAwesomeIcon
+            icon={faSliders}
+            className={settingsShow ? `transition rotate-90` : `transition`}
+          />
         </button>
         <input
-          className="text-center px-3 py-1 m-1 text-black shadow-md
+          id="query-field"
+          className="text-center px-3 py-1 m-1 text-black font-medium
                      rounded-r-lg bg-gray-100 hover:bg-gray-200 text-lg w-full"
           type="text"
           placeholder="type query..."
@@ -49,17 +155,36 @@ function Settings({ handleQuery, isEnter, query, handleClick }) {
           value={query}
         />
         <button
+          id="search-button"
           className="bg-sky-700 hover:bg-sky-900  text-center 
           px-3 py-1 m-1 text-white rounded-lg text-lg shadow-md active:shadow-none"
-          onClick={handleClick}
+          onClick={handleFetchClick}
         >
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
-      <div
-        id="search-params"
-        className="w-full flex flex-row items-center"
-      ></div>
+      {settingsShow ? (
+        <div
+          id="search-params"
+          className="w-full max-h-[85vh] items-center m-1 overflow-auto"
+        >
+          <SettingsBlock
+            settingsValues={settingsValues}
+            setSettingsValues={setSettingsValues}
+            type="order"
+          />
+          <SettingsBlock
+            settingsValues={settingsValues}
+            setSettingsValues={setSettingsValues}
+            type="content"
+          />
+          <SettingsBlock
+            settingsValues={settingsValues}
+            setSettingsValues={setSettingsValues}
+            type="age"
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
@@ -67,7 +192,7 @@ function Settings({ handleQuery, isEnter, query, handleClick }) {
 function Card({ e }) {
   const [pressed, setPressed] = useState(false);
 
-  const handleClick = () => {
+  const handleExpand = () => {
     setPressed(!pressed);
   };
 
@@ -79,9 +204,9 @@ function Card({ e }) {
     >
       <div className="text-sm flex justify-between p-1">
         <span className="px-1 font-bold">{e.nb_downloads}</span>
-        <span>{e.creator_name}</span>
-        <span title={TYPES[e.media_type_id].title} className="px-1">
-          <FontAwesomeIcon icon={TYPES[e.media_type_id].icon} />
+        <span>{`${e.creator_name} | ${e.creator_id}`}</span>
+        <span title={CONTENT_TYPES[e.media_type_id - 1].title} className="px-1">
+          <FontAwesomeIcon icon={CONTENT_TYPES[e.media_type_id - 1].icon} />
         </span>
       </div>
       <Image
@@ -90,7 +215,7 @@ function Card({ e }) {
         width={e.thumbnail_width}
         height={e.thumbnail_height}
         alt={e.title}
-        onClick={handleClick}
+        onClick={handleExpand}
       />
       <div className="text-sm flex justify-around p-1">
         <span className="px-1">{e.creation_date.substr(0, 7)}</span>
@@ -104,11 +229,11 @@ function Card({ e }) {
         </div>
       ) : null}
       {pressed ? (
-        <div onClick={handleClick} className={`expand`}>
+        <div onClick={handleExpand} className={`expand`}>
           <FontAwesomeIcon icon={faChevronUp} />
         </div>
       ) : (
-        <div onClick={handleClick} className={`expand`}>
+        <div onClick={handleExpand} className={`expand`}>
           <FontAwesomeIcon icon={faChevronDown} />
         </div>
       )}
@@ -120,7 +245,7 @@ export default function Home() {
   const [resp, setResp] = useState();
   const [query, setQuery] = useState("");
 
-  const handleClick = async () => {
+  const handleFetchClick = async () => {
     try {
       const res = await fetch(`/api/get-files?search=${query}`);
       if (!res.ok) {
@@ -140,7 +265,7 @@ export default function Home() {
 
   const isEnter = (e) => {
     if (e.key === "Enter") {
-      handleClick();
+      handleFetchClick();
     }
   };
 
@@ -150,7 +275,7 @@ export default function Home() {
         handleQuery={handleQuery}
         isEnter={isEnter}
         query={query}
-        handleClick={handleClick}
+        handleFetchClick={handleFetchClick}
       />
       <div
         className="   text-center p-12 m-16 text-black 

f989765defc1219fc0e53813cc25ed6a48d0174b
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-08-06 13:31:57 +0100

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
-// Tell Font Awesome to skip adding the CSS automatically 
+// Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
-config.autoAddCss = false; 
+config.autoAddCss = false;
+
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }

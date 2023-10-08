14e0ba0fc80935ac6cc4dec8908220fe6290c4ba
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-10-08 14:30:50 +0100

    reading data from db test - done

--- src/pages/api/test-db-sql.js
+++ src/pages/api/test-db-sql.js
@@ -9,9 +9,7 @@ export default async function testConnection(req, res) {
     database: process.env.DB_SCHEME,
   });
   try {
-    const result = await db.query("SELECT * FROM test");
-    console.log(result);
-    const rows = result[0];
+    const [rows, fields] = await db.query("SELECT * FROM test");
 
     res.status(200).json({ message: rows });
   } catch (err) {}
--- src/pages/test-db/index.js
+++ src/pages/test-db/index.js
@@ -1,8 +1,29 @@
 import { useEffect, useState } from "react";
 
+function PrintObject({ d }) {
+  console.log("PrintObject" + d + "d:");
+  console.log(d);
+  if (Object.keys(d).length > 0) {
+    let printedArray = [];
+    let ind = `${d.id}`;
+    for (const [key, val] of Object.entries(d)) {
+      printedArray.push(
+        <div key={`${key}-${ind}`}>
+          {key}: {val}
+        </div>
+      );
+    }
+    return printedArray;
+  } else {
+    return <div>Empty response</div>;
+  }
+}
+
 export default function Page() {
   const [messageFinal, setMessageFinal] = useState("Didn't try to connect");
+  const [data, setData] = useState([]);
   async function tryDb() {
+    console.log("calling tryDB");
     try {
       const res = await fetch("api/test-db-sql");
 
@@ -13,16 +34,24 @@ export default function Page() {
       const { message } = await result;
       console.log(message);
       setMessageFinal(JSON.stringify(message));
+      setData(message);
     } catch (err) {
       console.log(err.message);
       setMessageFinal(err.message);
     }
   }
 
-  useEffect(() => {
-    console.log("calling trydb");
-    tryDb();
-  }, []);
+  return (
+    <>
+      <button onClick={tryDb} className="bg-neutral-200 p-2">
+        Get Data
+      </button>
 
-  return <div>{messageFinal}</div>;
+      <div>{messageFinal}</div>
+      <h1>Data:</h1>
+      {data.map((row, ind) => (
+        <PrintObject key={`row-${ind}`} d={row} />
+      ))}
+    </>
+  );
 }

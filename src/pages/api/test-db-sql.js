cb6ca38e8c1c0d088e932f99ed2522cd4d4c7924
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-10-08 15:19:00 +0100

    insert test

--- src/pages/api/test-db-sql.js
+++ src/pages/api/test-db-sql.js
@@ -9,8 +9,15 @@ export default async function testConnection(req, res) {
     database: process.env.DB_SCHEME,
   });
   try {
+    await db.query(
+      "INSERT INTO test (name, address) VALUES ('insertedFromAPI', 'reactnextjs')"
+    );
+
     const [rows, fields] = await db.query("SELECT * FROM test");
 
     res.status(200).json({ message: rows });
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
 }
